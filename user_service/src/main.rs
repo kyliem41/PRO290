502c2d050a8f11e4cfcc704d33393136611a5e81
ImAPersonIght <zomhorsethompson2656@gmail.com>
2024-04-30 11:31:07 -0600

    Added rocker endpoint and address env variable

--- user_service/src/main.rs
+++ user_service/src/main.rs
@@ -1,5 +1,23 @@
+#[macro_use]
+extern crate rocket;
+
 mod utils;
 
-fn main() {
-    println!("Hello, world!");
+use std::env;
+
+use serde::Serialize;
+
+#[launch]
+fn rocket() -> _ {
+    let address =  env::var("API_ADDRESS").unwrap();
+
+    let config = rocket::Config {
+        address: address.parse().unwrap(),
+        port: 8001,
+        ..Default::default()
+    };
+
+    let rocket_instance = rocket::custom(config);
+
+    rocket_instance
 }
