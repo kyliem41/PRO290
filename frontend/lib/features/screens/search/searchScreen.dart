962960be02916559de5685cbd2fbc5539b294a92
kyliem41 <krhodus@student.neumont.edu>
2024-05-15 10:53:50 -0600

    search

--- frontend/lib/features/screens/search/searchScreen.dart
+++ frontend/lib/features/screens/search/searchScreen.dart
@@ -1,33 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:frontend/features/screens/home/notifications/notification.dart';
+import 'package:frontend/features/screens/home/notifications/notificationsColumn.dart';
 import 'package:frontend/features/screens/search/customAppBar.dart';
+import 'package:frontend/features/screens/search/searchResultsColumn.dart';
 
 class SearchPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: customAppBar(),
-      body: Column(
+    return Center(
+      child: Row(
         children: [
-          Padding(
-            padding: EdgeInsets.only(top: 16, left: 16, right: 16),
-            child: TextField(
-              decoration: InputDecoration(
-                hintText: "search",
-                hintStyle: TextStyle(color: Colors.grey.shade600),
-                prefixIcon: Icon(
-                  Icons.search,
-                  color: Colors.grey.shade600,
-                  size: 20,
-                ),
-                filled: true,
-                fillColor: Colors.grey.shade100,
-                contentPadding: EdgeInsets.all(8),
-                enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(20),
-                    borderSide: BorderSide(color: Colors.grey.shade100)),
-              ),
+          Expanded(
+            child: Container(
+              child: SearchResultsColumn(),
             ),
           ),
+          SizedBox(
+            width: MediaQuery.of(context).size.width * 0.25,
+            child: Container(
+              color: Colors.white,
+              child: NotificationColumn(),
+            ),
+          )
         ],
       ),
     );
