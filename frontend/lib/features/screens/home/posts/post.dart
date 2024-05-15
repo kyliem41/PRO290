d92d960e29f022ffbf8c725a26d5e8d8de63bf9b
kyliem41 <krhodus@student.neumont.edu>
2024-05-14 21:52:23 -0600

    backup

--- frontend/lib/features/screens/home/posts/post.dart
+++ frontend/lib/features/screens/home/posts/post.dart
@@ -3,109 +3,213 @@ import 'package:flutter/material.dart';
 class Post extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
+    return Container(
       height: 200,
-      child: Container(
-        padding: EdgeInsets.all(10),
-        margin: EdgeInsets.all(15),
-        decoration: BoxDecoration(
-          color: Theme.of(context).cardColor,
-          border: Border.all(style: BorderStyle.solid),
-          borderRadius: BorderRadius.circular(10),
-        ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            CircleAvatar(
-              child: Text("U"),
-            ),
-            Expanded(
-              child: SingleChildScrollView(
-                child: _postContent("User", "Handle", "Text"),
-              ),
-            ),
-          ],
-        ),
+      padding: EdgeInsets.all(10),
+      margin: EdgeInsets.all(15),
+      decoration: BoxDecoration(
+        color: Theme.of(context).cardColor,
+        border: Border.all(style: BorderStyle.solid),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          CircleAvatar(
+            child: Text("U"),
+          ),
+          Expanded(
+            child: _postContent("User", "Handle", "Text"),
+          ),
+        ],
       ),
     );
   }
 }
 
 Widget _postContent(String user, String userHandle, String text) {
-  //final DateTime time = new DateTime(2024);
   String time = "01/05/05";
   String location = "im right here";
 
-  return Flexible(
-    child: SingleChildScrollView(
-      child: Container(
-        margin: EdgeInsets.only(left: 10),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.start,
+  return Container(
+    margin: EdgeInsets.only(left: 10),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: <Widget>[
+        Row(
           children: <Widget>[
-            Row(
-              children: <Widget>[
-                Text(user,
-                    style: TextStyle(
-                        color: Colors.black, fontWeight: FontWeight.bold)),
-                Container(
-                  margin: EdgeInsets.only(left: 5),
-                  child: Text(" " + userHandle + " " + location + " " + time,
-                      style:
-                          TextStyle(color: Color.fromARGB(197, 189, 189, 189))),
-                ),
-              ],
-            ),
+            Text(user,
+                style: TextStyle(
+                    color: Colors.black, fontWeight: FontWeight.bold)),
             Container(
-                margin: EdgeInsets.only(top: 15),
-                child: Text(
-                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ultrices vitae auctor eu augue ut lectus. Purus non enim praesent elementum facilisis leo vel. Dictum fusce ut placerat orci nulla pellentesque dignissim. Mattis enim ut tellus elementum sagittis vitae. Tristique senectus et netus et malesuada fames ac turpis egestas. Malesuada fames ac turpis egestas integer eget aliquet nibh praesent. Non odio euismod lacinia at quis risus. Porta lorem mollis aliquam ut. Proin fermentum leo vel orci porta. Elementum nisi quis eleifend quam adipiscing vitae. Ut venenatis tellus in metus vulputate eu scelerisque felis imperdiet. Lectus urna duis convallis convallis tellus id interdum velit.',
-                    style: TextStyle(color: Colors.black))), //text
-            SizedBox(height: 10),
-            Container(
-              margin: EdgeInsets.only(left: 15, right: 15, top: 15, bottom: 10),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Row(
-                    children: <Widget>[
-                      Icon(Icons.message_rounded, color: Colors.black),
-                      Container(
-                        margin: EdgeInsets.only(left: 3),
-                        child:
-                            Text("15", style: TextStyle(color: Colors.black)),
-                      ),
-                    ],
-                  ),
-                  Row(
-                    children: <Widget>[
-                      Icon(Icons.repeat, color: Colors.black),
-                      Container(
-                        margin: EdgeInsets.only(left: 3),
-                        child:
-                            Text("15", style: TextStyle(color: Colors.black)),
-                      ),
-                    ],
-                  ),
-                  Row(
-                    children: <Widget>[
-                      Icon(Icons.favorite_border, color: Colors.black),
-                      Container(
-                        margin: EdgeInsets.only(left: 3),
-                        child:
-                            Text("15", style: TextStyle(color: Colors.black)),
-                      ),
-                    ],
-                  ),
-                  Icon(Icons.share, color: Colors.black),
-                ],
-              ),
+              margin: EdgeInsets.only(left: 5),
+              child: Text(" " + userHandle + " " + location + " " + time,
+                  style: TextStyle(color: Color.fromARGB(197, 189, 189, 189))),
             ),
           ],
         ),
-      ),
+        Container(
+            margin: EdgeInsets.only(top: 15),
+            child: Text(
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ultrices vitae auctor eu augue ut lectus. Purus non enim praesent elementum facilisis leo vel. Dictum fusce ut placerat orci nulla pellentesque dignissim. Mattis enim ut tellus elementum sagittis vitae. Tristique senectus et netus et malesuada fames ac turpis egestas. Malesuada fames ac turpis egestas integer eget aliquet nibh praesent. Non odio euismod lacinia at quis risus. Porta lorem mollis aliquam ut. Proin fermentum leo vel orci porta. Elementum nisi quis eleifend quam adipiscing vitae. Ut venenatis tellus in metus vulputate eu scelerisque felis imperdiet. Lectus urna duis convallis convallis tellus id interdum velit.',
+                style: TextStyle(color: Colors.black))), //text
+        SizedBox(height: 10),
+        Container(
+          margin: EdgeInsets.only(left: 15, right: 15, top: 15, bottom: 10),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  Icon(Icons.message_rounded, color: Colors.black),
+                  Container(
+                    margin: EdgeInsets.only(left: 3),
+                    child:
+                        Text("15", style: TextStyle(color: Colors.black)),
+                  ),
+                ],
+              ),
+              Row(
+                children: <Widget>[
+                  Icon(Icons.repeat, color: Colors.black),
+                  Container(
+                    margin: EdgeInsets.only(left: 3),
+                    child:
+                        Text("15", style: TextStyle(color: Colors.black)),
+                  ),
+                ],
+              ),
+              Row(
+                children: <Widget>[
+                  Icon(Icons.favorite_border, color: Colors.black),
+                  Container(
+                    margin: EdgeInsets.only(left: 3),
+                    child:
+                        Text("15", style: TextStyle(color: Colors.black)),
+                  ),
+                ],
+              ),
+              Icon(Icons.share, color: Colors.black),
+            ],
+          ),
+        ),
+      ],
     ),
   );
 }
+
+
+
+// import 'package:flutter/material.dart';
+
+// class Post extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return SizedBox(
+//       height: 200,
+//       child: Container(
+//         padding: EdgeInsets.all(10),
+//         margin: EdgeInsets.all(15),
+//         decoration: BoxDecoration(
+//           color: Theme.of(context).cardColor,
+//           border: Border.all(style: BorderStyle.solid),
+//           borderRadius: BorderRadius.circular(10),
+//         ),
+//         child: Row(
+//           mainAxisAlignment: MainAxisAlignment.start,
+//           crossAxisAlignment: CrossAxisAlignment.start,
+//           children: <Widget>[
+//             CircleAvatar(
+//               child: Text("U"),
+//             ),
+//             Expanded(
+//               child: SingleChildScrollView(
+//                 child: _postContent("User", "Handle", "Text"),
+//               ),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+// Widget _postContent(String user, String userHandle, String text) {
+//   //final DateTime time = new DateTime(2024);
+//   String time = "01/05/05";
+//   String location = "im right here";
+
+//   return Flexible(
+//     child: SingleChildScrollView(
+//       child: Container(
+//         margin: EdgeInsets.only(left: 10),
+//         child: Column(
+//           crossAxisAlignment: CrossAxisAlignment.start,
+//           mainAxisAlignment: MainAxisAlignment.start,
+//           children: <Widget>[
+//             Row(
+//               children: <Widget>[
+//                 Text(user,
+//                     style: TextStyle(
+//                         color: Colors.black, fontWeight: FontWeight.bold)),
+//                 Container(
+//                   margin: EdgeInsets.only(left: 5),
+//                   child: Text(" " + userHandle + " " + location + " " + time,
+//                       style:
+//                           TextStyle(color: Color.fromARGB(197, 189, 189, 189))),
+//                 ),
+//               ],
+//             ),
+//             Container(
+//                 margin: EdgeInsets.only(top: 15),
+//                 child: Text(
+//                     'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ultrices vitae auctor eu augue ut lectus. Purus non enim praesent elementum facilisis leo vel. Dictum fusce ut placerat orci nulla pellentesque dignissim. Mattis enim ut tellus elementum sagittis vitae. Tristique senectus et netus et malesuada fames ac turpis egestas. Malesuada fames ac turpis egestas integer eget aliquet nibh praesent. Non odio euismod lacinia at quis risus. Porta lorem mollis aliquam ut. Proin fermentum leo vel orci porta. Elementum nisi quis eleifend quam adipiscing vitae. Ut venenatis tellus in metus vulputate eu scelerisque felis imperdiet. Lectus urna duis convallis convallis tellus id interdum velit.',
+//                     style: TextStyle(color: Colors.black))), //text
+//             SizedBox(height: 10),
+//             Container(
+//               margin: EdgeInsets.only(left: 15, right: 15, top: 15, bottom: 10),
+//               child: Row(
+//                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//                 children: <Widget>[
+//                   Row(
+//                     children: <Widget>[
+//                       Icon(Icons.message_rounded, color: Colors.black),
+//                       Container(
+//                         margin: EdgeInsets.only(left: 3),
+//                         child:
+//                             Text("15", style: TextStyle(color: Colors.black)),
+//                       ),
+//                     ],
+//                   ),
+//                   Row(
+//                     children: <Widget>[
+//                       Icon(Icons.repeat, color: Colors.black),
+//                       Container(
+//                         margin: EdgeInsets.only(left: 3),
+//                         child:
+//                             Text("15", style: TextStyle(color: Colors.black)),
+//                       ),
+//                     ],
+//                   ),
+//                   Row(
+//                     children: <Widget>[
+//                       Icon(Icons.favorite_border, color: Colors.black),
+//                       Container(
+//                         margin: EdgeInsets.only(left: 3),
+//                         child:
+//                             Text("15", style: TextStyle(color: Colors.black)),
+//                       ),
+//                     ],
+//                   ),
+//                   Icon(Icons.share, color: Colors.black),
+//                 ],
+//               ),
+//             ),
+//           ],
+//         ),
+//       ),
+//     ),
+//   );
+// }
--- frontend/lib/features/screens/home/posts/postsColumn.dart
+++ frontend/lib/features/screens/home/posts/postsColumn.dart
@@ -11,37 +11,38 @@ class _PostsColumnState extends State<PostsColumn> {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        AppBar(
-          automaticallyImplyLeading: false,
-          title: const Text('Following',
-              style: TextStyle(
-                  fontSize: 28,
-                  fontWeight: FontWeight.bold,
-                  color: Color.fromRGBO(0, 121, 107, 1))),
-          centerTitle: true,
-          backgroundColor: Colors.teal.shade100,
-        ),
-        SizedBox(height: 20),
-        ListView.builder(
-          padding: EdgeInsets.only(top: 60, bottom: 20, left: 20, right: 20),
-          itemBuilder: (_, int index) => Post(),
-          itemCount: 10,
-          reverse: false,
-        ),
-        Positioned(
-          bottom: 16.0,
-          right: 16.0,
-          child: FloatingActionButton(
-            onPressed: () {
-              _overlayEntry = AddPostOverlayEntry(removeOverlayEntry).build();
-              Overlay.of(context)?.insert(_overlayEntry!);
-            },
-            child: Icon(Icons.edit_note_rounded),
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: const Text('Following',
+            style: TextStyle(
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+                color: Color.fromRGBO(0, 121, 107, 1))),
+        centerTitle: true,
+        backgroundColor: Colors.teal.shade100,
+      ),
+      body: Column(
+        children: [
+          SizedBox(height: 20),
+          Expanded(
+            child: ListView.builder(
+              padding: EdgeInsets.only(left: 20, right: 20),
+              itemBuilder: (_, int index) => Post(),
+              itemCount: 10,
+              reverse: false,
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _overlayEntry = AddPostOverlayEntry(removeOverlayEntry).build();
+          Overlay.of(context)?.insert(_overlayEntry!);
+        },
+        child: Icon(Icons.edit_note_rounded),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
     );
   }
 
@@ -80,54 +81,53 @@ class AddPost extends StatelessWidget {
         child: Material(
           color: Colors.transparent,
           child: Center(
-            child: SingleChildScrollView(
-              child: Padding(
-                padding: const EdgeInsets.all(20),
-                child: DecoratedBox(
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(10),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.grey.withOpacity(0.5),
-                        spreadRadius: 3,
-                        blurRadius: 5,
-                        offset: Offset(0, 3),
+            child: Padding(
+              padding: const EdgeInsets.all(20),
+              child: DecoratedBox(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 3,
+                      blurRadius: 5,
+                      offset: Offset(0, 3),
+                    ),
+                  ],
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          IconButton(
+                            onPressed: onRemove,
+                            icon: Icon(Icons.close),
+                          ),
+                        ],
+                      ),
+                      TextFormField(
+                        decoration: InputDecoration(
+                          prefixIcon:
+                              Icon(Icons.arrow_forward_ios_rounded),
+                          labelText: 'new post',
+                          hintText: "what's on your mind?",
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
+                      SizedBox(height: 35),
+                      SizedBox(
+                        width: 200,
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          child: Text('ADD'),
+                        ),
                       ),
                     ],
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(20),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.end,
-                      children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          children: [
-                            IconButton(
-                              onPressed: onRemove,
-                              icon: Icon(Icons.close),
-                            ),
-                          ],
-                        ),
-                        TextFormField(
-                          decoration: InputDecoration(
-                            prefixIcon: Icon(Icons.arrow_forward_ios_rounded),
-                            labelText: 'new post',
-                            hintText: "what's on your mind?",
-                            border: OutlineInputBorder(),
-                          ),
-                        ),
-                        SizedBox(height: 35),
-                        SizedBox(
-                          width: 200,
-                          child: ElevatedButton(
-                            onPressed: () {},
-                            child: Text('ADD'),
-                          ),
-                        ),
-                      ],
-                    ),
                   ),
                 ),
               ),
@@ -138,3 +138,146 @@ class AddPost extends StatelessWidget {
     );
   }
 }
+
+
+
+// import 'package:flutter/material.dart';
+// import 'package:frontend/features/screens/home/posts/post.dart';
+
+// class PostsColumn extends StatefulWidget {
+//   @override
+//   _PostsColumnState createState() => _PostsColumnState();
+// }
+
+// class _PostsColumnState extends State<PostsColumn> {
+//   OverlayEntry? _overlayEntry;
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Stack(
+//       children: [
+//         AppBar(
+//           automaticallyImplyLeading: false,
+//           title: const Text('Following',
+//               style: TextStyle(
+//                   fontSize: 28,
+//                   fontWeight: FontWeight.bold,
+//                   color: Color.fromRGBO(0, 121, 107, 1))),
+//           centerTitle: true,
+//           backgroundColor: Colors.teal.shade100,
+//         ),
+//         SizedBox(height: 20),
+//         ListView.builder(
+//           padding: EdgeInsets.only(top: 60, bottom: 20, left: 20, right: 20),
+//           itemBuilder: (_, int index) => Post(),
+//           itemCount: 10,
+//           reverse: false,
+//         ),
+//         Positioned(
+//           bottom: 16.0,
+//           right: 16.0,
+//           child: FloatingActionButton(
+//             onPressed: () {
+//               _overlayEntry = AddPostOverlayEntry(removeOverlayEntry).build();
+//               Overlay.of(context)?.insert(_overlayEntry!);
+//             },
+//             child: Icon(Icons.edit_note_rounded),
+//           ),
+//         ),
+//       ],
+//     );
+//   }
+
+//   void removeOverlayEntry() {
+//     if (_overlayEntry != null) {
+//       _overlayEntry!.remove();
+//       _overlayEntry = null;
+//     }
+//   }
+// }
+
+// class AddPostOverlayEntry {
+//   final VoidCallback removeOverlayEntry;
+
+//   AddPostOverlayEntry(this.removeOverlayEntry);
+
+//   OverlayEntry build() {
+//     return OverlayEntry(
+//       builder: (context) => AddPost(removeOverlayEntry),
+//     );
+//   }
+// }
+
+// class AddPost extends StatelessWidget {
+//   final VoidCallback onRemove;
+//   AddPost(this.onRemove);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Center(
+//       child: Container(
+//         constraints: BoxConstraints(
+//           maxWidth: 800,
+//           maxHeight: 500,
+//         ),
+//         child: Material(
+//           color: Colors.transparent,
+//           child: Center(
+//             child: SingleChildScrollView(
+//               child: Padding(
+//                 padding: const EdgeInsets.all(20),
+//                 child: DecoratedBox(
+//                   decoration: BoxDecoration(
+//                     color: Colors.white,
+//                     borderRadius: BorderRadius.circular(10),
+//                     boxShadow: [
+//                       BoxShadow(
+//                         color: Colors.grey.withOpacity(0.5),
+//                         spreadRadius: 3,
+//                         blurRadius: 5,
+//                         offset: Offset(0, 3),
+//                       ),
+//                     ],
+//                   ),
+//                   child: Padding(
+//                     padding: const EdgeInsets.all(20),
+//                     child: Column(
+//                       crossAxisAlignment: CrossAxisAlignment.end,
+//                       children: [
+//                         Row(
+//                           mainAxisAlignment: MainAxisAlignment.end,
+//                           children: [
+//                             IconButton(
+//                               onPressed: onRemove,
+//                               icon: Icon(Icons.close),
+//                             ),
+//                           ],
+//                         ),
+//                         TextFormField(
+//                           decoration: InputDecoration(
+//                             prefixIcon: Icon(Icons.arrow_forward_ios_rounded),
+//                             labelText: 'new post',
+//                             hintText: "what's on your mind?",
+//                             border: OutlineInputBorder(),
+//                           ),
+//                         ),
+//                         SizedBox(height: 35),
+//                         SizedBox(
+//                           width: 200,
+//                           child: ElevatedButton(
+//                             onPressed: () {},
+//                             child: Text('ADD'),
+//                           ),
+//                         ),
+//                       ],
+//                     ),
+//                   ),
+//                 ),
+//               ),
+//             ),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
