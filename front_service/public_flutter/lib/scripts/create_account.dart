a9bdd6e6d0d8676593b7275c5a9366b35d096067
ImAPersonIght <zomhorsethompson2656@gmail.com>
2024-05-20 10:16:37 -0600

    commiting for main and to move into my branch

--- front_service/public_flutter/lib/scripts/create_account.dart
+++ front_service/public_flutter/lib/scripts/create_account.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'dart:io';
 
 Future<String> validateUsername(String username) async {
   final response = await http.get(Uri.parse('http://localhost:80/user/get/username/$username'));
@@ -41,6 +42,33 @@ String validatePassword(String password, String confirmationPassword){
   return "Passwords don't match";
 }
 
-void createAccount (String username, String dob, String email, String password) {
+Future<void> createAccount(String username, String dob, String email, String password, File file) async {
+  var uri = Uri.parse('http://localhost:80/user/post');
+  var request = http.MultipartRequest('POST', uri);
 
+  request.fields['username'] = username;
+  request.fields['dob'] = dob;
+  request.fields['email'] = email;
+  request.fields['password'] = password;
+
+  var fileStream = http.ByteStream(file.openRead());
+  var length = await file.length();
+  var multipartFile = http.MultipartFile(
+    "TempFile<'r>", // This should match the server's expected parameter name
+    fileStream,
+    length,
+    filename: file.path.split('/').last,
+  );
+  request.files.add(multipartFile);
+
+  var response = await request.send();
+
+  if (response.statusCode == 200) {
+    var responseBody = await response.stream.bytesToString();
+    print('Response: $responseBody');
+    // Handle successful response
+  } else {
+    print('Failed to create account: ${response.statusCode}');
+    // Handle error response
+  }
 }
