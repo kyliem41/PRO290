01cc7e163fb1a3ecac71d05c20580f8b0b350f25
kyliem41 <krhodus@student.neumont.edu>
2024-05-29 11:22:45 -0600

    ok

---
+++ frontend/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=C:\y2\q3\serviceBased2\PRO290\flutter\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\y2\q3\serviceBased2\PRO290\frontend"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=0.0.1"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
